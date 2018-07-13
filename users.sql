bd028781089655f2a6f1bb1430166c15a2993d69
jadzia7 <7jadzia7@gmail.com>
2018-07-13 13:34:16 +0200

    Update users.sql

--- users.sql
+++ users.sql
@@ -8,7 +8,7 @@ CREATE TABLE `users` (
   `id` int(11) NOT NULL,
   `fname` varchar(50) NOT NULL,
   `lname` varchar(50) NOT NULL,
-  `email` varchar(50) NOT NULL,
+  `email` varchar(70) NOT NULL,
   `name` blob,
   `address` varchar(150)  NOT NULL,
   `phone` varchar(15) NOT NULL
