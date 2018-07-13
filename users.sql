f0d178bd694f7bd0457a7b0071c0f290c94be1c1
jadzia7 <7jadzia7@gmail.com>
2018-07-13 13:33:51 +0200

    Add files via upload

---
+++ users.sql
@@ -0,0 +1,16 @@
+
+
+--
+-- Struktura tabeli dla tabeli `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `fname` varchar(50) NOT NULL,
+  `lname` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `name` blob,
+  `address` varchar(150)  NOT NULL,
+  `phone` varchar(15) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
